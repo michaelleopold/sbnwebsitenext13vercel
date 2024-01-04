b68f6165633681a1d08f8c310da9f0271a4d23be
michael leopold tinangon <michaelleopold2016@gmail.com>
2024-01-04 15:36:04 +0700

    change from www to without www

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Fokus Utama Di Bidang Pemompaan Tambang Dengan Pengalaman 20 Tahun Lebih - PT Samudera Biru Nusantara',
   description: 'Berfokus di bidang pemompaan dewatering/air dan slurry/lumpur di tambang batubara selama kurang lebih 20 tahun, PT Samudera Biru Nusantara telah bekerja di banyak tambang besar seperti PT Wijaya Karja, PT Berau Coal, PT Dharma Henwa, dan masih banyak lagi',
-  metadataBase: new URL('https://www.samuderabirunusantara.com'),
+  metadataBase: new URL('https://samuderabirunusantara.com'),
 };
 
 export default function RootLayout({ children }) {
