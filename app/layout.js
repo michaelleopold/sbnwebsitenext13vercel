8dfd450b9844bc51844e4533cf377c635a100580
michael leopold tinangon <michaelleopold2016@gmail.com>
2024-01-25 15:46:57 +0700

    change metadata in layout, make it the same as metadata in homepage, delete font Inter in layout now we will use roboto to all text from tailwind

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,15 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export const metadata = {
-  title: 'Fokus Utama Di Bidang Pemompaan Tambang Dengan Pengalaman 20 Tahun Lebih - PT Samudera Biru Nusantara',
-  description: 'Berfokus di bidang pemompaan dewatering/air dan slurry/lumpur di tambang batubara selama kurang lebih 20 tahun, PT Samudera Biru Nusantara telah bekerja di banyak tambang besar seperti PT Wijaya Karja, PT Berau Coal, PT Dharma Henwa, dan masih banyak lagi',
+  title: 'Solusi Pompa Dewatering dan Slurry Dengan Pengalaman 20 Tahun Lebih - PT Samudera Biru Nusantara',
+  description: 'SBN bergerak di bidang pemompaan dewatering/air dan slurry/lumpur di tambang batubara sejak tahun 2000 (24 tahun), kami telah bekerja di banyak tambang besar seperti PT Wijaya Karja, PT Berau Coal, PT Dharma Henwa, dan masih banyak lagi',
   metadataBase: new URL('https://samuderabirunusantara.com'),
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="in">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
