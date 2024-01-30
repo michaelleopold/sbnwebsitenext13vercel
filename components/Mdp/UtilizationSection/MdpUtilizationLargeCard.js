43f26aaba1ce65fcf6824cdd1895143a90d4049e
michael leopold tinangon <michaelleopold2016@gmail.com>
2024-01-30 16:19:57 +0700

    fix Mdp Utilization Large Card width

--- components/Mdp/UtilizationSection/MdpUtilizationLargeCard.js
+++ components/Mdp/UtilizationSection/MdpUtilizationLargeCard.js
@@ -7,7 +7,7 @@ const MdpUtilizationLargeCard = ({
     <div className="relative drop-shadow-xl h-96 xl:h-100 2xl:h-110 w-170 xl:w-[50rem] 2xl:w-[57rem] rounded-xl overflow-hidden z-0">
       {image && <Image src={image} placeholder="blur" sizes={sizes} alt={alt} fill className="object-cover brightness-[0.82]" /> }
     </div>
-    <h1 className={`absolute ${top} ${topXl} ${top2Xl} left-7 text-white font-semibold text-[1.75rem] xl:text-[1.8rem] 2xl:text-[2rem] leading-9 ${maxWdith} 2xl:max-w-[36rem] z-10`}>{description}</h1>
+    <h1 className={`absolute ${top} ${topXl} ${top2Xl} left-7 text-white font-semibold text-[1.75rem] xl:text-[1.8rem] 2xl:text-[2rem] leading-9 w-full ${maxWdith} 2xl:max-w-[36rem] z-10`}>{description}</h1>
   </div>
 );
 
